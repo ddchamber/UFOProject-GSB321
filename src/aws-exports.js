f3821b8edb50e5eda7916e70e41740210035711c
ddchamber <danielc8889@gmail.com>
2025-03-06 15:50:05 -0800

    Add files via upload

---
+++ src/aws-exports.js
@@ -0,0 +1,11 @@
+const awsconfig = {
+    Auth: {
+      // Your AWS configuration here
+    },
+    Storage: {
+      bucket: 'ufodata2', // your S3 bucket name
+      region: 'us-east-2', // your region
+    },
+  };
+  
+  export default awsconfig;
